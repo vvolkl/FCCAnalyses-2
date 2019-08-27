78b67868b485a82c0465c0a45925bfe051ed8c35
Valentin Volkl <valentin.volkl@cern.ch>
2019-08-27 15:32:30 +0200

    tmp

--- FCCeeAnalyses/ZH_Zmumu/dataframe/fcc_ana_ZH_Zmumu.cxx
+++ FCCeeAnalyses/ZH_Zmumu/dataframe/fcc_ana_ZH_Zmumu.cxx
@@ -47,6 +47,10 @@ std::vector<fcc::LorentzVector> recoil (std::vector<fcc::ParticleData> in) {
     return result;
 }
 
+auto recoil_add_to_dataframe( ROOT::RDataFrame& df, std::string input_column, std::string output_column) {
+  return df.Define(output_column, recoil, {input_column});
+}
+
 std::vector<fcc::ParticleData>  selectLeptons (std::vector<fcc::ParticleData> in, std::vector<fcc::TaggedParticleData> iso) {
   std::vector<fcc::ParticleData> result;
   result.reserve(in.size());
@@ -55,11 +59,14 @@ std::vector<fcc::ParticleData>  selectLeptons (std::vector<fcc::ParticleData> in
     if (std::sqrt(std::pow(p.core.p4.px,2) + std::pow(p.core.p4.py,2)) > 20) {
       if (iso[i].tag  < 0.4) {
         result.emplace_back(p);
-
       }
     }
   }
   return result;
+}
+
+auto select_leptons_add_to_dataframe( ROOT::RDataFrame& df, std::string input_column, std::string input_column_iso, std::string output_column) {
+  return df.Define(output_column, selectLeptons, {input_column, input_column_iso});
 }
 
 std::vector<fcc::JetData> selectJetsBs (std::vector<fcc::JetData> in, std::vector<fcc::TaggedJetData> btags) {
@@ -76,6 +83,10 @@ std::vector<fcc::JetData> selectJetsBs (std::vector<fcc::JetData> in, std::vecto
   return result;
 }
 
+auto selectJetsBs_add_to_dataframe( ROOT::RDataFrame& df, std::string input_column, std::string input_column_btags, std::string output_column) {
+  return df.Define(output_column, selectJetsBs, {input_column, input_column_btags});
+}
+
 // @todo: refactor to remove code duplication with selectJetsBs
 std::vector<fcc::JetData> selectJetsLights (std::vector<fcc::JetData> in, std::vector<fcc::TaggedJetData> btags) {
   std::vector<fcc::JetData> result;
@@ -90,6 +101,151 @@ std::vector<fcc::JetData> selectJetsLights (std::vector<fcc::JetData> in, std::v
   }
   return result;
 }
+
+auto selectJetsLights_add_to_dataframe( ROOT::RDataFrame& df, std::string input_column, std::string input_column_btags, std::string output_column) {
+  return df.Define(output_column, selectJetsLights, {input_column, input_column_btags});
+}
+
+std::vector<fcc::JetData> noMatchJets (std::vector<fcc::JetData> in, std::vector<fcc::ParticleData> matchParticles) {
+  std::vector<fcc::JetData> result;
+  result.reserve(in.size());
+  for (int i = 0; i < in.size(); ++i) {
+    auto & p = in[i];
+    bool matched = false;
+    for (int j = 0; j < matchParticles.size(); ++j) {
+      auto & matchCandidate = matchParticles[j];
+      if (deltaR(p.core.p4, matchCandidate.core.p4) < 0.2) {
+        matched = true;
+      }
+    }
+    if (matched == false) {
+      result.emplace_back(p);
+    }
+  }
+  return result;
+}
+
+std::vector<float> get_pt(std::vector<fcc::ParticleData> in){
+ std::vector<float> result;
+ for (int i = 0; i < in.size(); ++i) {
+   result.push_back(sqrt(in[i].core.p4.px * in[i].core.p4.px + in[i].core.p4.py * in[i].core.p4.py));
+ }
+ return result;
+}
+
+
+std::vector<fcc::ParticleData> mergeElectronsAndMuons(std::vector<fcc::ParticleData> x, std::vector<fcc::ParticleData> y) {
+  std::vector<fcc::ParticleData> result;
+  result.reserve(x.size() + y.size());
+  result.insert( result.end(), x.begin(), x.end() );
+  result.insert( result.end(), y.begin(), y.end() );
+  return result;
+}
+
+///@todo: comment!
+std::vector<fcc::ParticleData> LeptonicZBuilder(std::vector<fcc::ParticleData> leptons) {
+  std::vector<fcc::ParticleData> result;
+  int n = leptons.size();
+  if (n >2) {
+    // all possible combinations of 2 particles from the set  'leptons'
+    std::vector<bool> v(n);
+    std::fill(v.end() - 2, v.end(), true);
+    do {
+      fcc::ParticleData zed;
+      zed.core.pdgId = 23;
+      TLorentzVector zed_lv; 
+      for (int i = 0; i < n; ++i) {
+          if (v[i]) {
+            zed.core.charge += leptons[i].core.charge;
+            TLorentzVector lepton_lv;
+            lepton_lv.SetXYZM(leptons[i].core.p4.px, leptons[i].core.p4.py, leptons[i].core.p4.pz, leptons[i].core.p4.mass);
+            zed_lv += lepton_lv;
+          }
+      }
+      zed.core.p4.px = zed_lv.Px();
+      zed.core.p4.py = zed_lv.Py();
+      zed.core.p4.pz = zed_lv.Pz();
+      zed.core.p4.mass = zed_lv.M();
+      result.emplace_back(zed);
+    } while (std::next_permutation(v.begin(), v.end()));
+  }
+  return result;
+}
+
+/// @todo: refactor to remove code duplication with leptonicZBuilder
+std::vector<fcc::ParticleData> LeptonicHiggsBuilder(std::vector<fcc::ParticleData> leptons) {
+  std::vector<fcc::ParticleData> result;
+  int n = leptons.size();
+  if (n >2) {
+    std::vector<bool> v(n);
+    std::fill(v.end() - 2, v.end(), true);
+    do {
+      fcc::ParticleData zed;
+      zed.core.pdgId = 25;
+      TLorentzVector zed_lv; 
+      for (int i = 0; i < n; ++i) {
+          if (v[i]) {
+            zed.core.charge += leptons[i].core.charge;
+            TLorentzVector lepton_lv;
+            lepton_lv.SetXYZM(leptons[i].core.p4.px, leptons[i].core.p4.py, leptons[i].core.p4.pz, leptons[i].core.p4.mass);
+            zed_lv += lepton_lv;
+          }
+      }
+      zed.core.p4.px = zed_lv.Px();
+      zed.core.p4.py = zed_lv.Py();
+      zed.core.p4.pz = zed_lv.Pz();
+      zed.core.p4.mass = zed_lv.M();
+      result.emplace_back(zed);
+    } while (std::next_permutation(v.begin(), v.end()));
+  }
+  if (result.size() > 1) {
+    auto  higgsresonancesort = [] (fcc::ParticleData i ,fcc::ParticleData j) { return (abs( 125. -i.core.p4.mass)<abs(125.-j.core.p4.mass)); };
+    std::sort(result.begin(), result.end(), higgsresonancesort);
+    std::vector<fcc::ParticleData>::const_iterator first = result.begin();
+    std::vector<fcc::ParticleData>::const_iterator last = result.begin() + 1;
+    std::vector<fcc::ParticleData> onlyBestHiggs(first, last);
+    return onlyBestHiggs;
+  } else {
+    return result;
+  }
+}
+
+std::vector<float> id_float(std::vector<fcc::FloatValueData> x) {
+  std::vector<float> result;
+  for (auto & p: x) {
+    result.push_back(p.value);
+  }
+  return result;
+}
+
+std::vector<float> get_mass(std::vector<fcc::ParticleData> x) {
+  std::vector<float> result;
+  for (auto & p: x) {
+    result.push_back(p.core.p4.mass);
+  }
+  return result;
+}
+
+int get_nparticles(std::vector<fcc::ParticleData> x) {
+  int result =  x.size();
+  return result;
+}
+
+int get_njets(std::vector<fcc::JetData> x) {
+  int result =  x.size();
+  return result;
+}
+
+int get_njets2(std::vector<fcc::JetData> x, std::vector<fcc::JetData> y) {
+  int result =  x.size() + y.size();
+  return result;
+}
+
+auto add_to_dataframe( ROOT::RDataFrame& df, std::string input_column, std::string output_column) {
+  return df.Define(output_column, get_njets2, {input_column});
+
+}
+
 
 
 
@@ -129,155 +285,8 @@ int main(int argc, char* argv[]){
 
   
 
-  auto noMatchJets = [](std::vector<fcc::JetData> in, std::vector<fcc::ParticleData> matchParticles) {
-    std::vector<fcc::JetData> result;
-    result.reserve(in.size());
-    for (int i = 0; i < in.size(); ++i) {
-      auto & p = in[i];
-      bool matched = false;
-      for (int j = 0; j < matchParticles.size(); ++j) {
-        auto & matchCandidate = matchParticles[j];
-        if (deltaR(p.core.p4, matchCandidate.core.p4) < 0.2) {
-          matched = true;
-        }
-      }
-      if (matched == false) {
-        result.emplace_back(p);
-      }
-    }
-    return result;
 
 
-    };
-
-
-   auto get_pt = [](std::vector<fcc::ParticleData> in){
-     std::vector<float> result;
-       for (int i = 0; i < in.size(); ++i) {
-         result.push_back(sqrt(in[i].core.p4.px * in[i].core.p4.px + in[i].core.p4.py * in[i].core.p4.py));
-       }
-       return result;
-   };
-
-
-   auto mergeElectronsAndMuons = [](std::vector<fcc::ParticleData> x, std::vector<fcc::ParticleData> y) {
-     std::vector<fcc::ParticleData> result;
-     result.reserve(x.size() + y.size());
-     result.insert( result.end(), x.begin(), x.end() );
-     result.insert( result.end(), y.begin(), y.end() );
-     return result;
-
-   };
-
-  auto LeptonicZBuilder = [](std::vector<fcc::ParticleData> leptons) {
-
-        std::vector<fcc::ParticleData> result;
-        int n = leptons.size();
-        if (n >2) {
-          std::vector<bool> v(n);
-          std::fill(v.end() - 2, v.end(), true);
-          do {
-            fcc::ParticleData zed;
-            zed.core.pdgId = 23;
-            TLorentzVector zed_lv; 
-            for (int i = 0; i < n; ++i) {
-                if (v[i]) {
-                  zed.core.charge += leptons[i].core.charge;
-                  TLorentzVector lepton_lv;
-                  lepton_lv.SetXYZM(leptons[i].core.p4.px, leptons[i].core.p4.py, leptons[i].core.p4.pz, leptons[i].core.p4.mass);
-                  zed_lv += lepton_lv;
-                }
-            }
-            zed.core.p4.px = zed_lv.Px();
-            zed.core.p4.py = zed_lv.Py();
-            zed.core.p4.pz = zed_lv.Pz();
-            zed.core.p4.mass = zed_lv.M();
-            result.emplace_back(zed);
-          
-          } while (std::next_permutation(v.begin(), v.end()));
-        }
-
-    return result;
-  };
-
-  /// @todo: refactor to remove code duplication with leptonicZBuilder
-  auto LeptonicHiggsBuilder = [](std::vector<fcc::ParticleData> leptons) {
-
-        std::vector<fcc::ParticleData> result;
-        int n = leptons.size();
-        if (n >2) {
-          std::vector<bool> v(n);
-          std::fill(v.end() - 2, v.end(), true);
-          do {
-            fcc::ParticleData zed;
-            zed.core.pdgId = 25;
-            TLorentzVector zed_lv; 
-            for (int i = 0; i < n; ++i) {
-                if (v[i]) {
-                  zed.core.charge += leptons[i].core.charge;
-                  TLorentzVector lepton_lv;
-                  lepton_lv.SetXYZM(leptons[i].core.p4.px, leptons[i].core.p4.py, leptons[i].core.p4.pz, leptons[i].core.p4.mass);
-                  zed_lv += lepton_lv;
-                }
-            }
-            zed.core.p4.px = zed_lv.Px();
-            zed.core.p4.py = zed_lv.Py();
-            zed.core.p4.pz = zed_lv.Pz();
-            zed.core.p4.mass = zed_lv.M();
-            result.emplace_back(zed);
-
-          
-          } while (std::next_permutation(v.begin(), v.end()));
-        }
-
-    if (result.size() > 1) {
-    auto  higgsresonancesort = [] (fcc::ParticleData i ,fcc::ParticleData j) { return (abs( 125. -i.core.p4.mass)<abs(125.-j.core.p4.mass)); };
-    std::sort(result.begin(), result.end(), higgsresonancesort);
-
-    std::vector<fcc::ParticleData>::const_iterator first = result.begin();
-    std::vector<fcc::ParticleData>::const_iterator last = result.begin() + 1;
-    std::vector<fcc::ParticleData> onlyBestHiggs(first, last);
-    return onlyBestHiggs;
-    } else {
-    return result;
-    }
-  };
-
-  auto id_float = [](std::vector<fcc::FloatValueData> x) {
-    std::vector<float> result;
-    for (auto & p: x) {
-      result.push_back(p.value);
-
-    }
-    return result;
-  };
-
-  auto get_mass = [](std::vector<fcc::ParticleData> x) {
-    std::vector<float> result;
-    for (auto & p: x) {
-      result.push_back(p.core.p4.mass);
-    }
-
-    return result;
-  };
-
-  auto get_nparticles = [](std::vector<fcc::ParticleData> x) {
-    int result =  x.size();
-    return result;
-  };
-
-  auto get_njets = [](std::vector<fcc::JetData> x) {
-    int result =  x.size();
-    return result;
-
-
-  };
-
-  auto get_njets2 = [](std::vector<fcc::JetData> x, std::vector<fcc::JetData> y) {
-    int result =  x.size() + y.size();
-    return result;
-
-  };
 
 
 
