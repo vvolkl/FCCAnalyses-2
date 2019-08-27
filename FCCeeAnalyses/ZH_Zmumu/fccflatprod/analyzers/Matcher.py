8267c248466866d49485ff0a4a329540a9e64843
Valentin Volkl <valentin.volkl@cern.ch>
2019-08-28 01:14:51 +0200

    tmp

--- FCCeeAnalyses/ZH_Zmumu/fccflatprod/analyzers/Matcher.py
+++ FCCeeAnalyses/ZH_Zmumu/fccflatprod/analyzers/Matcher.py
@@ -1,4 +1,14 @@
+
+import ROOT
+from ROOT import noMatchJets_add_to_dataframe
+
 class Matcher:
   def __init__(*args, **kwargs):
+    self.delta_r = kwargs["delta_r"]
+    self.match_particles = kwargs["match_particles"]
+    self.particles = kwargs["particles"]
+
+  def doit(self, dataframe):
+    noMatchJets_add_to_dataframe(dataframe, self.particles)
     pass
 
--- FCCeeAnalyses/ZH_Zmumu/fccflatprod/analyzers/Selector.py
+++ FCCeeAnalyses/ZH_Zmumu/fccflatprod/analyzers/Selector.py
@@ -1,3 +1,15 @@
+import ROOT
+from ROOT import select_leptons_add_to_dataframe
+
+
 class Selector:
-  def __init__(*args, **kwargs):
+  def __init__(self, *args, **kwargs):
+    self.output = kwargs["output"]
+    self.input_objects =  kwargs["input_objects"]
+    self.filter_func = kwargs["filter_func"]
+
+  def doit(self, dataframe):
+    #select_leptons_add_to_dataframe(dataframe)
     pass
+    
+    
